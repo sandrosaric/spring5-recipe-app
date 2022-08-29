ba5a95f652fc3d051850f59d3d53aafcaf6d234c
sandrosaric <sandro.saric@gmail.com>
2022-08-29 23:15:44 +0200

    OneTOOne,OneToMany,MaayToOne JPA Relationships, Cascade persistance...

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5recipeapp.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+}
